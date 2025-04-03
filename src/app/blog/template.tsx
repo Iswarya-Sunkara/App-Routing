b9ddaa60a5e4508df7958a72ece5558118edc00d
Iswarya-Sunkara <sunkara.iswarya@softsuave.org>
2025-04-03 11:58:53 +0530

    deleted

--- src/app/blog/template.tsx
+++
@@ -1,9 +0,0 @@
-export default function Template({ children }: { children: React.ReactNode }) {
-    return (
-      <div style={{ border: "2px solid red", padding: "1rem" }}>
-        <h2>Fresh Template Loaded!</h2>
-        {children}
-      </div>
-    );
-  }
-  
