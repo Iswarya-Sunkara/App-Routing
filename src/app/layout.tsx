df824d13a7a64e14b28d0de91ff5af198483a8bf
Iswarya-Sunkara <sunkara.iswarya@softsuave.org>
2025-04-03 15:39:04 +0530

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <body>{children}</body>
       <body>
         <header style={{backgroundColor:"lightblue",padding:"1rem",}}>
           <p>Header</p>
